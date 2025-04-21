7c462984610f9a0abaef6879041109efab8d6ae4
HoyteP89 <hoytepatrick89@gmail.com>
2025-04-21 14:14:17 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   root: './',
-  publicDir: 'public', // specify the public directory
+  publicDir: 'public', // specifies where the public directory is
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-  }
-})
+  },
+});
