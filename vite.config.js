f88716cd274cb73e5294919bfd7c64fe059e5803
hoyte <Apple@MacBook-Air.local>
2025-04-21 12:17:51 -0400

    Save local changes before pulling

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   root: './',
+<<<<<<< HEAD
   publicDir: 'public', // specify the public directory
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+=======
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+>>>>>>> 020b259 (Initial commit with Tailwind CSS setup)
   }
 })
